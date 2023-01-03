6d62ac249b695545d949cc695e277d617d8b0c8d
Fewrick <54175840+Fewrick@users.noreply.github.com>
2023-01-03 13:38:42 +0100

    KNAPPEPERERR!!!!

--- src/Controller/Gui.java
+++ src/Controller/Gui.java
@@ -3,6 +3,8 @@ package Controller;
 import java.io.InputStream;
 import java.util.stream.IntStream;
 
+import javax.sql.rowset.spi.SyncResolver;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
@@ -10,23 +12,24 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Gui extends Application {
 
-    static void initGui(){
+    static void initGui() {
         launch(new String[] {});
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        // initialisering af grundpanel
+        // init af grundpanel
         stage.setTitle("Reversi");
         StackPane root = new StackPane();
-        stage.setScene(new Scene(root,500,500));
+        stage.setScene(new Scene(root, 800, 800));
         stage.show();
         stage.setMinHeight(400);
         stage.setMinWidth(400);
@@ -36,22 +39,44 @@ public class Gui extends Application {
         root.getChildren().add(panel_manager);
 
         // init af gridpane
-        GridPane board = new GridPane();
+        GridPane board = initBoard();
         panel_manager.setCenter(board);
 
-        // init af imageview 
-        InputStream inStream = getClass().getResourceAsStream("/Assets/stoneTileEmpty.jpg");
-        Image pic = new Image(inStream);
-        
+    }
+
+    private GridPane initBoard() {
+        GridPane board = new GridPane();
+
+        // init af tiles
+        InputStream empty_tile_src = getClass().getResourceAsStream("/Assets/stoneTileEmpty.jpg");
+        Image empty_tile = new Image(empty_tile_src);
+        InputStream white_tile_src = getClass().getResourceAsStream("/Assets/stoneTileWhite.jpg");
+        Image white_tile = new Image(white_tile_src);
+        InputStream black_tile_src = getClass().getResourceAsStream("/Assets/stoneTileBlack.jpg");
+        Image black_tile = new Image(black_tile_src);
+
         // init af spilbræt
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                ImageView imv = new ImageView();
-                imv.setImage(pic);
-                board.add(imv,i,j);
+                Tile tile = new Tile(i, j);
+                tile.setImage(empty_tile);
+                board.add(tile, i, j);
             }
-            
+        }
+        return board;
+    }
+
+    class Tile extends ImageView {
+        private int positionX;
+        private int positionY;
+
+        public Tile(int x, int y) {
+            this.positionX = x;
+            this.positionY = y;
+            setOnMouseClicked(e -> {
+                System.out.println("x: " + positionX + "y: " + positionY);
+            });
         }
     }
-    
+
 }
