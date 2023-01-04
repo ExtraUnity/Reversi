0928d39aa2aacf925acc34abd647ab10ede29ecb
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-01-04 10:51:39 +0100

    Flips bricks

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.ArrayList;
+
 import MsgPass.ControllerMsg.ControllerWindowClosedMsg;
 import MsgPass.ControllerMsg.UpdateBoardMsg;
 import MsgPass.ModelMsg.TilePressedMsg;
@@ -80,23 +82,88 @@ public class Game {
      * brikker der er blevet vendt
      */
     void handleTileClick(TilePosition pos) {
-        var thiscolor = nextturn;
-        if (followRules() && !legalMove(pos, thiscolor)) {
+        if (isColor(pos.x, pos.y) && board[pos.x][pos.y] == null) {
+            System.out.println("Illegal move at " + pos + ". Tile already colored");
             return;
         }
 
-        board[pos.x][pos.y] = thiscolor;
-        switch (thiscolor) {
-            case WHITE:
-                nextturn = TileColor.BLACK;
-                break;
-            case BLACK:
-                nextturn = TileColor.WHITE;
-                break;
+        var thiscolor = nextturn;
+        var flippedTiles = flipTiles(pos, thiscolor);
+        if (followRules() && flippedTiles.size() == 0) {
+            System.out.println("Illegal move at " + pos + ". No flips");
+            return;
+        }
+
+        flippedTiles.add(pos);
+
+        for (var t_pos : flippedTiles) {
+            board[t_pos.x][t_pos.y] = thiscolor;
         }
 
         turns++;
-        Model.sendControllerMsg(new UpdateBoardMsg(thiscolor, new TilePosition[] { pos }));
+        Model.sendControllerMsg(new UpdateBoardMsg(thiscolor, flippedTiles.toArray(new TilePosition[0])));
+        nextturn = nextturn.switchColor();
+    }
+
+    boolean isColor(int x, int y) {
+        return x >= 0 && x < 8 && y >= 0 && y < 8 && board[x][y] != null;
+    }
+
+    ArrayList<TilePosition> flipable(TilePosition pos, int dx, int dy, TileColor placedColor) {
+        var flipped = new ArrayList<TilePosition>();
+        int x = pos.x + dx;
+        int y = pos.y + dy;
+        boolean flipValid;
+        while (true) {
+            if (!isColor(x, y)) {
+                flipValid = false;
+                break;
+            }
+            if (board[x][y].otherColor(placedColor)) {
+                flipped.add(new TilePosition(x, y));
+                x += dx;
+                y += dy;
+            } else {
+                flipValid = true;
+                break;
+            }
+        }
+        if (flipValid) {
+            for (var flip_pos : flipped) {
+                System.out.println(pos + " dir " + dx + " " + dy + " flipped " + flip_pos);
+            }
+            return flipped;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    ArrayList<TilePosition> flipTiles(TilePosition pos, TileColor placedColor) {
+        // Flip all above
+        var aboveFlipped = flipable(pos, 0, 1, placedColor);
+        var rightFlipped = flipable(pos, 1, 0, placedColor);
+        var belowFlipped = flipable(pos, 0, -1, placedColor);
+        var leftFlipped = flipable(pos, -1, 0, placedColor);
+
+        var topRightFlipped = flipable(pos, 1, -1, placedColor);
+        var botRightFlipped = flipable(pos, 1, 1, placedColor);
+
+        var topLeftFlipped = flipable(pos, -1, -1, placedColor);
+        var botLeftFlipped = flipable(pos, -1, 1, placedColor);
+
+        var allFlipped = new ArrayList<TilePosition>();
+        allFlipped.addAll(aboveFlipped);
+        allFlipped.addAll(rightFlipped);
+        allFlipped.addAll(belowFlipped);
+        allFlipped.addAll(leftFlipped);
+
+        allFlipped.addAll(topRightFlipped);
+        allFlipped.addAll(botRightFlipped);
+        allFlipped.addAll(topLeftFlipped);
+        allFlipped.addAll(botLeftFlipped);
+
+        return allFlipped;
+
     }
 
     boolean legalMove(TilePosition pos, TileColor color) {
