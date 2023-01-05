43ad268290dd17d657612482523a425607398251
Fewrick <54175840+Fewrick@users.noreply.github.com>
2023-01-05 13:46:04 +0100

    Update Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -69,7 +69,7 @@ public class Controller {
                     public void run() {
                         Gui.buildGui();
                     }
-                    
+
                 });
             }
         }
