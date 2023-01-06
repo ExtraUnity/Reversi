415a7ff87a2ecad621d5fb11159e0a978dcc1e54
Christian <cvedelpetersen@gmail.com>
2023-01-06 11:32:24 +0100

    removed ugly lines

--- src/Controller/Gui/MenuSide.java
+++ src/Controller/Gui/MenuSide.java
@@ -4,18 +4,13 @@ import Shared.TileColor;
 import Model.GameOptions;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.paint.Color;
 
 public class MenuSide extends BorderPane {
     MenuSide() {
         Gui.stackRoot.widthProperty().addListener((obs, oldVal, newVal) -> {
             updateSize();
         });
-        setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
