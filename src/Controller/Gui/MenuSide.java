6ca420fc762c23bdb1087a648b6e7b99c5d9e9f2
Fewrick <54175840+Fewrick@users.noreply.github.com>
2023-01-09 10:47:14 +0100

    Update MenuSide.java

--- src/Controller/Gui/MenuSide.java
+++ src/Controller/Gui/MenuSide.java
@@ -35,7 +35,7 @@ class MenuRight extends MenuSide {
             setMargin(getTop(), new Insets(5));
         }
 
-        setBottom(new TurnIndication(PlayerCharacter.Black, gameOptions.startPlayer == TileColor.BLACK));
+        setBottom(new TurnIndication(PlayerCharacter.Stalin, gameOptions.startPlayer == TileColor.BLACK));
         setAlignment(getBottom(), Pos.BOTTOM_CENTER);
     }
 }
@@ -46,7 +46,7 @@ class MenuLeft extends MenuSide {
             setCenter(new PointCounter(TileColor.WHITE));
             setMargin(getCenter(), new Insets(5));
         }
-        setTop(new TurnIndication(PlayerCharacter.White, gameOptions.startPlayer == TileColor.WHITE));
+        setTop(new TurnIndication(PlayerCharacter.BarakObama, gameOptions.startPlayer == TileColor.WHITE));
         setAlignment(getTop(), Pos.TOP_CENTER);
 
     }
