5fc2562f286e81ded66751cd2c55fe7593997c26
Pinkymus <katinkaspangtoft@gmail.com>
2023-01-10 11:37:55 +0100

    merged

--- src/Controller/Gui/ButtonMultiplayer.java
+++ src/Controller/Gui/ButtonMultiplayer.java
@@ -3,7 +3,15 @@ package Controller.Gui;
 import Model.GameOptions;
 import Model.Model;
 import Model.Game.GameMode;
+import Server.ServerConn;
 import Shared.TileColor;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class ButtonMultiplayer extends Button {
 
@@ -15,9 +23,29 @@ public class ButtonMultiplayer extends Button {
             setImage(imgPressed);
         });
         setOnMouseReleased(e -> {
-            Model.startGame(GameMode.MULTIPLAYER, new GameOptions(-1, false, TileColor.WHITE));
+            final Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.initOwner(Gui.stage);
+                VBox dialogVbox = new VBox(20);
+
+                ServerConn conn = new ServerConn();
+
+                dialogVbox.getChildren().add(new Text("Your id: " + conn.netId));
+                var inputfield = new TextField();
+                dialogVbox.getChildren().add(inputfield);
+                var submitButton = new javafx.scene.control.Button("Join");
+                submitButton.setOnMouseClicked(new EventHandler<javafx.event.Event>() {
+                    @Override
+                    public void handle(javafx.event.Event arg0) {
+                        conn.tryJoin(inputfield.getText());
+                    }
+                });
+                dialogVbox.getChildren().add(submitButton);
+                Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                dialog.setScene(dialogScene);
+                dialog.show();
             setImage(img);
         });
     }
-    
+
 }
