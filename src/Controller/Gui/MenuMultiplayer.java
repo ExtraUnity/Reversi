19e0f143bede26e4ef92fcc044213743da674101
Pinkymus <katinkaspangtoft@gmail.com>
2023-01-10 22:39:11 +0100

    multiplayer menu made pretty-ish

--- src/Controller/Gui/MenuMultiplayer.java
+++ src/Controller/Gui/MenuMultiplayer.java
@@ -2,26 +2,36 @@ package Controller.Gui;
 
 import Server.ServerConn;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 
 public class MenuMultiplayer extends BorderPane {
 
     public MenuMultiplayer(ServerConn conn){
-        VBox dialogVbox = new VBox(20);
+        VBox dialogVbox = new VBox();
+        Text key = new Text("Your id: " + conn.netId);
         TextField text = new TextField();
-        ButtonJoin join = new ButtonJoin(conn , text);   
 
-        text.setMaxWidth(Gui.fitTileSize());
-        
-        dialogVbox.getChildren().add(new Text("Your id: " + conn.netId));
+        text.setMaxWidth(Gui.fitTileSize()*2);
+        text.setFont(Font.font("verdana", FontWeight.BLACK, FontPosture.REGULAR, 20));
+
+        key.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+    
+        dialogVbox.getChildren().add(key);
         dialogVbox.getChildren().add(text);
-        dialogVbox.getChildren().add(join);
-        
-        setCenter(dialogVbox);
+        dialogVbox.getChildren().add(new ButtonJoin(conn , text));
+        dialogVbox.getChildren().add(new ButtonExitGame());
 
+        dialogVbox.setAlignment(Pos.CENTER);
+        dialogVbox.setSpacing(15);
+        setCenter(dialogVbox);
+        
         setMargin(getCenter(), new Insets(64, 0, 0, 0));
     }
     
