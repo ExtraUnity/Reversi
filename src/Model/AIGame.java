cdcc4904b562d559e87277c519113c40e574324d
Christian <cvedelpetersen@gmail.com>
2023-01-11 22:44:26 +0100

    Moved function around

--- src/Model/AIGame.java
+++ src/Model/AIGame.java
@@ -57,4 +57,46 @@ public class AIGame extends Game {
         }
     }
 
+    public LegalMove miniMax(TileColor[][] board, LegalMove[] legalMoves, int depth, boolean maximizingPlayer,
+            LegalMove madeMove) {
+        if (depth == 0 || legalMoves.length == 0) {
+            madeMove.setEvaluation(evaluatePosition(board));
+            return madeMove;
+        }
+
+        // IMPLEMENT IF LEGALMOVES == 0
+
+        if (maximizingPlayer) { // wants to minimize evaluation - Black player
+            LegalMove minEval = new LegalMove(new TilePosition(0, 0), 0, 1_000_000); // every found evaluation should
+                                                                                     // be better than this
+            for (LegalMove move : legalMoves) {
+                TileColor[][] tempBoard = copyBoard(board);
+                tempBoard[move.position.x][move.position.y] = TileColor.BLACK;
+                LegalMove eval = miniMax(tempBoard, Game.getAllLegalMoves(TileColor.WHITE, tempBoard), depth - 1,
+                        false, move);
+                move.setEvaluation(eval.evaluation);
+                if (move.compareTo(minEval) < 0) {
+                    minEval = move;
+                }
+            }
+            return minEval;
+        }
+
+        // White pieces
+        LegalMove maxEval = new LegalMove(new TilePosition(0, 0), 0, -1_000_000); // every found evaluation should be
+        // better than this
+        for (LegalMove move : legalMoves) {
+            TileColor[][] tempBoard = copyBoard(board);
+            tempBoard[move.position.x][move.position.y] = TileColor.WHITE;
+            LegalMove eval = miniMax(tempBoard, Game.getAllLegalMoves(TileColor.BLACK, tempBoard), depth - 1,
+                    true, move);
+            move.setEvaluation(eval.evaluation);
+            if (move.compareTo(maxEval) > 0) {
+                maxEval = move;
+            }
+        }
+        return maxEval;
+
+    }
+
 }
