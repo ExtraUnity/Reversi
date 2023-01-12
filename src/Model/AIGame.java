98fae541e71cb078efdbd949257246fa565a32c2
Christian <cvedelpetersen@gmail.com>
2023-01-12 15:51:05 +0100

    Fixed bug with player being able to control AI's move

--- src/Model/AIGame.java
+++ src/Model/AIGame.java
@@ -9,11 +9,13 @@ import MsgPass.ControllerMsg.WinnerMsg;
 
 public class AIGame extends Game {
     static AIPlayer aiPlayer;
+    private static boolean isCalculating;
 
     AIGame(GameOptions options) {
         super(options, GameMode.AI_GAME);
         aiPlayer = new AIPlayer(board);
         aiPlayer.setMyTurn(options.startPlayer == TileColor.BLACK);
+        isCalculating = false;
     }
 
     @Override
@@ -26,8 +28,11 @@ public class AIGame extends Game {
 
     @Override
     boolean handleTileClick(TilePosition pos, TilePressedMsg msg) {
+        if (isCalculating) {
+            return false;
+        }
         boolean superReturn = super.handleTileClick(pos, msg);
-        if (followRules() && getNextTurn() == TileColor.BLACK && !allTilesPlaced()) {
+        if (followRules() && superReturn && !allTilesPlaced() && getNextTurn() == TileColor.BLACK) {
             var legalMoves = getAllLegalMoves(getNextTurn(), board);
             handleAITurn(legalMoves);
         }
@@ -36,8 +41,11 @@ public class AIGame extends Game {
 
     @Override
     boolean handlePassClick(PassMsg msg) {
+        if (isCalculating) {
+            return false;
+        }
         boolean superReturn = super.handlePassClick(msg);
-        if (followRules() && getNextTurn() == TileColor.BLACK) {
+        if (followRules() && superReturn && getNextTurn() == TileColor.BLACK) {
             var legalMoves = getAllLegalMoves(getNextTurn(), board);
             handleAITurn(legalMoves);
         }
@@ -46,7 +54,7 @@ public class AIGame extends Game {
 
     @Override
     void handleAITurn(LegalMove[] legalMoves) {
-
+        isCalculating = true;
         aiPlayer.updateBoard(board, legalMoves); // updates with the newest board
 
         // TEMPORARY
@@ -60,9 +68,11 @@ public class AIGame extends Game {
         if (legalMoves.length != 0) {
             aiPlayer.setBestMove(); // Algorithm magic
             System.out.println("Best Move is: " + aiPlayer.getBestMove());
+            isCalculating = false;
+            handleTileClick(aiPlayer.getBestMove(), new TilePressedMsg(aiPlayer.getBestMove()));
+            // Model.sendGameMsg(new TilePressedMsg(aiPlayer.getBestMove()));
 
-            Model.sendGameMsg(new TilePressedMsg(aiPlayer.getBestMove()));
-            // handleTileClick(aiPlayer.getBestMove());
+            //
         } else { // then pass
             System.out.println("I should pass...");
             Model.sendGameMsg(new PassMsg());
