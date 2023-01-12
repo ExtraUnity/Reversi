6c3200854767292e7b205826f1ac97416f4c2fbb
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-01-12 11:36:12 +0100

    multiplyer timer virker

---
+++ src/Server/GameTimeOptionNetmsg.java
@@ -0,0 +1,12 @@
+package Server;
+
+import MsgPass.ModelMsg.ModelMsg;
+
+public class GameTimeOptionNetmsg extends ModelMsg {
+    final int gameTime;
+
+    GameTimeOptionNetmsg(int gameTime) {
+        this.gameTime = gameTime;
+    }
+
+}
