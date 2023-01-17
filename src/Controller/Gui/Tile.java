c6b1fdcbaaeae49b682f967332f6dd6bc0aa6332
Pinkymus <katinkaspangtoft@gmail.com>
2023-01-17 16:13:05 +0100

    Clean Up

--- src/Controller/Gui/Tile.java
+++ src/Controller/Gui/Tile.java
@@ -8,7 +8,6 @@ import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
 public class Tile extends BorderPane {
@@ -18,8 +17,6 @@ public class Tile extends BorderPane {
     TileColor tilecolor = null;
 
     // init af tiles
-    static private Image white_tile;
-    static private Image black_tile;
     static private Image legal_tile;
     static private Image empty_tile;
 
@@ -50,25 +47,6 @@ public class Tile extends BorderPane {
         return empty_tile;
     }
 
-    /*
-    private Image getWhiteImage() {
-        if (white_tile == null) {
-            var white_tile_src = getClass().getResourceAsStream("/Assets/stoneTileWhite.png");
-            white_tile = new Image(white_tile_src, Gui.fitTileSize(), 0, true, false);
-
-        }
-        return white_tile;
-    }
-
-    private Image getBlackImage() {
-        if (black_tile == null) {
-            var black_tile_src = getClass().getResourceAsStream("/Assets/stoneTileBlack.png");
-            black_tile = new Image(black_tile_src, Gui.fitTileSize(), 0, true, false);
-        }
-        return black_tile;
-    }
-    */
-
     private Image getLegalImage() {
         if (legal_tile == null) {
             var legal_tile_src = getClass().getResourceAsStream("/Assets/stoneTilePossibleMove.png");
@@ -78,34 +56,6 @@ public class Tile extends BorderPane {
         return legal_tile;
     }
 
-    /*
-    private static URL whiteToBlack;
-    private static URL blackToWhite;
-
-    private Image getSwitchToWhite() {
-        if (blackToWhite == null) {
-            blackToWhite = getClass().getResource("/Assets/stoneTileBlackToWhite.gif");
-        }
-        try {
-            var switch_to_white = new Image(blackToWhite.openStream(), Gui.fitTileSize(), 0, true, false);
-            return switch_to_white;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    private Image getSwitchToBlack() {
-        if (whiteToBlack == null) {
-            whiteToBlack = getClass().getResource("/Assets/stoneTileWhiteToBlack.gif");
-        }
-        try {
-            var switch_to_black = new Image(whiteToBlack.openStream(), Gui.fitTileSize(), 0, true, false);
-            return switch_to_black;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    */
     private boolean isLegalMove = false;
 
     public void setLegalImage() {
@@ -120,35 +70,6 @@ public class Tile extends BorderPane {
         }
     }
 
-    //private volatile int animationIndex = 0;
-    /*
-    public void switchTilecolor(TileColor newColor) {
-        isLegalMove = false;
-        this.tilecolor = newColor;
-        if (newColor == TileColor.WHITE)
-            imageView.setImage(getSwitchToWhite());
-        else
-            imageView.setImage(getSwitchToBlack());
-
-        animationIndex += 1;
-        int newAnimationIndex = animationIndex;
-
-        new Thread(() -> {
-            try {
-                Thread.sleep(1000);
-                Platform.runLater(() -> {
-                    //For at garanterer at ting ikke fucker up
-                    if (animationIndex == newAnimationIndex) {
-                        setTilecolor(newColor);
-                    }
-                });
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
-
-    }
-    */
     public void setTilecolor(TileColor tilecolor) {
         isLegalMove = false;
         this.tilecolor = tilecolor;
