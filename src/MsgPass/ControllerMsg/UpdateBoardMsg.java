3d18725ee702300b8f327b5ccd82e7bbbff0d158
Christian <cvedelpetersen@gmail.com>
2023-01-18 13:42:49 +0100

    Removed unused file

--- src/MsgPass/ControllerMsg/UpdateBoardMsg.java
+++ src/MsgPass/ControllerMsg/UpdateBoardMsg.java
@@ -10,19 +10,23 @@ public class UpdateBoardMsg extends ControllerMsg {
     // Indeholder koordinater på de tiles som skal ændres, og den farve det skal
     // ændres til.
     public final TileColor color;
-    public TilePosition[] tilePositions;
+    public final TilePosition[] tilePositions;
     public final LegalMove[] legalMoves;
 
     public final int whitePoints;
     public final int blackPoints;
 
-    public boolean isPassing;
+    public final boolean isPassing;
     public final int turns;
 
     // Skrevet af Christian
     public UpdateBoardMsg(TileColor color, TilePosition[] tilePositions, LegalMove[] legalMoves, int whitePoints,
             int blackPoints, boolean isPassing, int turns) {
-        this(color, legalMoves, whitePoints, blackPoints, turns);
+        this.color = color;
+        this.legalMoves = legalMoves;
+        this.whitePoints = whitePoints;
+        this.blackPoints = blackPoints;
+        this.turns = turns;
         this.tilePositions = tilePositions;
         this.isPassing = isPassing;
 
@@ -30,13 +34,7 @@ public class UpdateBoardMsg extends ControllerMsg {
 
     // Skrevet af Thor
     public UpdateBoardMsg(TileColor color, LegalMove[] legalMoves, int whitePoints, int blackPoints, int turns) {
-        this.color = color;
-        this.tilePositions = new TilePosition[0];
-        this.legalMoves = legalMoves;
-        this.whitePoints = whitePoints;
-        this.blackPoints = blackPoints;
-        this.isPassing = true;
-        this.turns = turns;
+        this(color, new TilePosition[0], legalMoves, whitePoints, blackPoints, true, turns);
     }
 
     // Skrevet af Thor
