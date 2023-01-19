5d1a1afa425d63c64979230411935be3741d7dda
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-01-19 13:55:04 +0100

    dims

--- src/MsgPass/ModelMsg/PassMsg.java
+++ src/MsgPass/ModelMsg/PassMsg.java
@@ -1,11 +1,5 @@
 package MsgPass.ModelMsg;
 //Filen er skrevet af Christian
-import Shared.TilePosition;
 
 public class PassMsg extends ModelMsg {
-    public TilePosition pos;
-
-    public PassMsg() {
-        pos = new TilePosition(0, 0);
-    }
 }
